6a276a3b2b8e8dc1512a619d5183fae1755e005d
Samson33 <samsonmazeau@gmail.com>
2021-12-02 12:31:31 +0100

    comments and log 1/2

--- platform/my-vuln-app/client/src/components/Articles.vue
+++ platform/my-vuln-app/client/src/components/Articles.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { onMounted } from '@vue/runtime-core'
+import { onMounted, watch } from '@vue/runtime-core'
 import { reactive, ref } from 'vue'
 import axios from 'axios';
 
+const ip_serv = 'localhost:8181'
+
 const form = reactive({
     articles: [],
+    comment: [],
     art_content: [],
     art_auth: [],
     art_title: [],
@@ -13,43 +16,115 @@ const form = reactive({
     art_time: [],
     art_id: [],
     is_loading: true,
+    article: '',
     subject: '',
     email: '',
-    i: 0,
     message: '',
-    
 })
 
-const getArticles = () =>{
-    axios.get('http://localhost:8181/article')
+// const com = reactive({
+//     display: false,
+//     id: [],
+//     author: {},
+//     content: [],
+//     temporal: [],
+//     date: [],
+//     time: [],
+//     title: [],
+//     link_art_id: [],
+// })
+
+const getArticles = () => {
+    axios.get(`http://${ip_serv}/article`)
     .then(res => {
+        form.articles = res.data.articles
         for (let i = 0; i < res.data.articles.length; i++){
             form.art_temp[i] = res.data.articles[i].date 
             form.art_auth[i] = res.data.articles[i].author
             form.art_content[i] = res.data.articles[i].content 
             form.art_title[i] = res.data.articles[i].title
             form.art_id[i] = res.data.articles[i].id
-        }
+            const y = form.art_id[i]
+            
+            // get the comments:
+
+            axios.get(`http://${ip_serv}/article/${y}/comment`)
+            .then(res => {
+                form.comment[i] = res.data.commentaries
+                // com.id[i] = []
+                // com.temporal[i] = []
+                // com.author[i] = []
+                // com.content[i] = []
+                // com.title[i] = []
+                // com.link_art_id[i] = []
+                // for (var j = 0; j < res.data.commentaries.length; j++){
+                //     com.id[i][j] = res.data.commentaries[j].id
+                //     com.temporal[i].push(res.data.commentaries[i].date)
+                //     com.author[i][j] = res.data.commentaries[i].author
+                //     com.content[i][j] = res.data.commentaries[i].content 
+                //     com.title[i][j] = res.data.commentaries[i].title
+                //     com.link_art_id[i][j] = res.data.commentaries[i].article_id
+                // }
+            })
+        } 
         if (form.art_id.length !== 0){
             form.is_loading = false
             for (const x in form.art_temp){
                 const split = form.art_temp[x].split('T')
                 form.art_date[x] = split[0]
                 form.art_time[x] = split[1]
-                console.log(form.art_time)
             }
         }
     })
 }
 
-const increment = () => {
-    count.value += 1
-    console.log(count)
+const postComment = () => {
+    console.log(form.comment)
+    console.log(form.article)
+    const result = form.articles.find( article => article.title === form.article)
+    const l = form.comment[result.id-1].length + 1
+    const data = {        
+        'article_id': result.id,
+        'author': form.email,
+        'content' : form.message,
+        'date': "2021-11-30T21:42:55",
+        'id': l,
+        'title': form.subject,
+    }
+    axios.post(`http://${ip_serv}/article/${result.id}/comment`, data)
+    .then(res=>{
+        console.log(res.data)
+    })
 }
 
-onMounted(() => {
-  getArticles()
-})
+// const getComment = () => {
+//     com.display = true
+//     try {
+//         console.log(form.comment[0][1].title)
+//     } catch(r) {
+//         console.log('cela ne fonctionne pas')
+//     }
+
+// }
+    
+// const increment = () => {
+//     count.value += 1
+//     console.log(count)
+// }
+
+// setInterval(function(){ 
+//     getComment()
+// }, 5000);
+
+// onMounted(() => {
+//   getArticles()
+// })
+
+watch(
+  () => {
+    getArticles()
+  },
+)
 
 </script>
 
@@ -67,7 +142,7 @@ onMounted(() => {
             </el-image>
         </el-row>
         <el-row class="author">
-            <el-col :span="1.8">
+            <el-col :span="2.2">
                 <div>
                     Author:
                 </div>
@@ -79,21 +154,11 @@ onMounted(() => {
                     </i>
                 </div>
             </el-col>
-            <el-col :span="1.8">
-                <div>
-                    Date:
-                </div>
-            </el-col>
             <el-col :span="3">
                 <div>
                     <i>
                         {{form.art_date[index-1]}},
                     </i>
-                </div>
-            </el-col>
-            <el-col :span="1.5">
-                <div>
-                    Time:
                 </div>
             </el-col>
             <el-col :span="3">
@@ -104,42 +169,82 @@ onMounted(() => {
                 </div>
             </el-col>
         </el-row>
-        <br/>
         <el-row>
             <div>
                 {{form.art_content[index-1]}} 
             </div>
         </el-row>
-    <br/>
+        <br/>
+        <br/>
+        <div>
+            <el-row class="comment">
+                    <div>Comments:</div>
+                </el-row>
+            <br/>
+            <hr>
+            <br/>
+            <div v-for="jindex in form.comment[index-1].length" :key="jindex">                
+                <el-row :span="1.5">
+                    <el-col :span="1.8">
+                        <div>
+                            Title:  <i>{{form.comment[index-1][jindex-1].title}}.</i>
+                        </div>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="1.8">
+                        <div>
+                            Author: <i>{{form.comment[index-1][jindex-1].author}}.</i>
+                        </div>
+                    </el-col>
+                </el-row>
+                <br/>
+                <el-row>
+                    <div>
+                        {{form.comment[index-1][jindex-1].content}}.
+                    </div>
+                </el-row>
+                <br/>
+                <hr>
+                <br/>
+            </div>
+        </div>
+        <br/>
+        <br/>
     </div>
     <el-row class="comment">
-        <h3>Leave a comment :</h3>
+            <div>Leave a comment :</div>
+        </el-row>
+        <br/>
+        <el-row>
+            <div class="comments">
+                <el-form 
+                    ref="formulaire" 
+                    :model="form" 
+                    size="medium" 
+                    label-width="200px">
+                        <el-form-item class="textForm" label="your email" label-width="540px">
+                            <el-input v-model="form.email" placeholder="Enter your email"></el-input>
+                        </el-form-item>
+                        <el-form-item class="textForm" label="Name of the article" >
+                            <el-input v-model="form.article" placeholder="Name of the article you want ot comment"/>
+                        </el-form-item>
+                        <el-form-item class="textForm" label="Subject" >
+                            <el-input v-model="form.subject" placeholder="Enter your subject"/>
+                        </el-form-item>
+                        <el-form-item  class="textForm" label="Your message" prop="desc">
+                            <el-input v-model="form.message" type="textarea" placeholder="Write your message"></el-input>
+                        </el-form-item>
+                </el-form>
+            </div>
     </el-row>
-    <el-row>
-        <div>
-            <el-form 
-                ref="form" 
-                :model="form" 
-                size="medium" 
-                label-width="200px">
-                    <el-form-item class="textForm" label="Subject">
-                        <el-input :v-model="form.subject" clearable placeholder="Enter your subject"></el-input>
-                    </el-form-item>
-                    <el-form-item class="textForm" label="Email">
-                        <el-input v-model="form.email" clearable placeholder="Enter your email"></el-input>
-                    </el-form-item>
-                    <el-form-item  class="textForm" label="Your message" prop="desc">
-                        <el-input v-model="form.message" type="textarea" placeholder="Write your message"></el-input>
-                    </el-form-item>
-            </el-form>
-            <div>{{form.subject}}</div>
-        </div>
-    </el-row>
+    <div>
+        <el-button @click="postComment" class="post">Post comment</el-button>
+    </div>
 </div>
 </template>
 
 <style>
-
 .test{
     color:blue;
     left: 50px;
@@ -158,7 +263,18 @@ onMounted(() => {
 }
 
 .comment{
-    font-family: Bradley Hand, cursive;
+     font-family: Bradley Hand, cursive;
+     font-size: 20px;
+     font-family: Bradley Hand, cursive;
+}
+
+.comments{
+    text-align: center;
+}
+
+.post{
+    margin-top: 2.5em;
+    margin-bottom: 2.5em;
 }
 
 .textForm{
